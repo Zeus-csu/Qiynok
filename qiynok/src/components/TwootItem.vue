ed68d4db2d59952085a11f49f6e2c257806cf4cc
Zues-CSU <3368752532@qq.com>
2021-01-15 22:58:35 +0800

    added events

---
+++ qiynok/src/components/TwootItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="twoot-item">
+        <div class="user-profile-twoot">
+            <div class="twoot-item-user">
+                @{{username}}
+             </div>
+            <div class="twoot-item-content">
+                {{twoot.content}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TwootItem",
+        props: {
+          username:{
+            type: String,
+            required: true
+                 },
+          twoot: {
+            type: Object,
+            required: true
+               }
+        }
+    }
+</script>
+
+<style  scoped>
+
+.twoot-item {
+    border:1px solid #777;
+    background-color: ivory;
+}
+
+
+
+</style>
--- qiynok/src/components/UserProfile.vue
+++ qiynok/src/components/UserProfile.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="user-profile">
     <div class="user-panel">
-      <h1 class="user-name">{{ user.name }}</h1>
+      <h2 class="user-name">{{ fulName }}</h2>
+      <h3 class="user-name"> @{{ user.username}}</h3>
       <div class="user-follower-count">
-          Follower: {{follower}}
+          Follower: {{followers}}
+          <button @click="followUser"> Follow</button>
        </div>
-    </div>
+        
+     </div>   
+      <div>
+           <TwootItem class="twoots-panel" v-for="twoot in user.twoots" :key="twoot.id" :username="user.username" :twoot="twoot" />
+         </div>   
   </div>
+  
 </template>
 
 <script>
+import TwootItem from "./TwootItem.vue";
+
 export default {
   name: 'UserProfile',
+  componenets:{TwootItem},
   data() {
     return{
       followers:0,
@@ -21,7 +31,11 @@ export default {
         firstname: 'Salom',
         lastname: 'Alikov',
         email: 'zeus@mail.uz',
-        isAdmin:true
+        isAdmin:true,
+        twoots:[
+          {id:1, content:'First content there' } ,
+          {id:2, content: "Another content wrote down!"}
+        ]
       }
     }
   },
@@ -37,26 +51,40 @@ methods:{
   }
 },
 mounted(){
-  this.follower()
-}
+  this.followUser();
+},
 
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.user-profile{
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 120%;
+  
+ 
+  padding: 50px 5%;
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.user-panel{
+  
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: #aff;
+  border: solid 1px rgb(63, 47, 47);
+  border-radius: 5px;
+
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+h2{
+  margin: 5px 0;
 }
-a {
-  color: #42b983;
-}
+
+
+
+
 </style>
